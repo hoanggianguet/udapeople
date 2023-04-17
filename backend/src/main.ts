dca9961668098bcbce6d50593db61879a724b649
hoanggianguet <gianghoang.uet.vnu@gmail.com>
2023-04-18 03:09:54 +0700

    Revert "Update smoke test stage"
    This reverts commit 6a1d8ff3334702bba9579485b731c6a1301503cf.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
       }
     },
   };
-  app.use(cors({  "origin": "*", "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"}));
+  app.use(cors(corsOptions));
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
